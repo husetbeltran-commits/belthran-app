6e135eaf970c441a506179157229c6b2920edc45
Ricard <huset.beltran@gmail.com>
2025-12-01 17:00:43 +0100

    Fix mainImageUrl path in mockData

--- data/mockData.ts
+++ data/mockData.ts
@@ -219,7 +219,7 @@ Han bjuder in dig till att lära känna honom, inte bara som en symbol, utan som
     tags: ['Teologi', 'Jesus', 'Vem är Jesus-serien'],
     createdAt: '2025-12-01',
     featured: false,
-    mainImageUrl: 'public/images/vem-ar-jesus-v2.png'
+    mainImageUrl: '/public/images/vem-ar-jesus-v2.png'
   },
   {
   id: '4',
